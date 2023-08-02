dbbb43840b917fa607ab3a45e2c6ba7349f1a4f6
aditazad <aditazad0008@gmail.com>
2023-08-02 22:04:39 +0600

    Initial commit

---
+++ Assignment.dart
@@ -0,0 +1,21 @@
+void main(){
+  int a = 10;
+  int b = 15;
+  print("a = 10, b = 15");
+
+  int c = a+b;
+  print("Addition: 10+15 = $c");
+
+  int d = a-b;
+  print("Subtraction: 10-15 = $d");
+
+  int e = a*b;
+  print("Multiplication: 10*15 = $e");
+
+  double f = a/b;
+  print("Division: 10/15 = $f");
+
+  int g = a%b;
+  print("Modulus: 10%15 = $g");
+
+}
